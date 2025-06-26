9e49e7355710c2dbedf58a67ec6fdfeea89a1982
KaykySiq <josekayky@hotmail.com>
2025-06-26 16:25:31 -0300

    feat(funcionario): integrar funções da entidade Funcionario.

---
+++ src/main/java/com/ibametro/folha_ponto/domain/repositories/FuncionarioRepository.java
@@ -0,0 +1,14 @@
+package com.ibametro.folha_ponto.domain.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.ibametro.folha_ponto.domain.Funcionario;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+    Optional<Funcionario> findByEmployeeId(String employeeId);
+
+}
