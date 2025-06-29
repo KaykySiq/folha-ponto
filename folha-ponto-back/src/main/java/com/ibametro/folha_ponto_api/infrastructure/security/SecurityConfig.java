6c881fd1800d5f64e68c1e24ee808504b1769e85
KaykySiq <josekayky@hotmail.com>
2025-06-29 14:08:25 -0300

    feat: adiciona o SecurityConfig.

---
+++ folha-ponto-back/src/main/java/com/ibametro/folha_ponto_api/infrastructure/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.ibametro.folha_ponto_api.infrastructure.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
