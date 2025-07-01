1b93ad519cccecd2a1fe8b54f1ed4264844baf62
KaykySiq <josekayky@hotmail.com>
2025-07-01 19:34:12 -0300

    feat: adiciona o enum de meses.

---
+++ folha-ponto-front/src/constants/meses.ts
@@ -0,0 +1,14 @@
+export const MESES_REFERENCIA = [
+  { value: "JANEIRO", label: "Janeiro" },
+  { value: "FEVEREIRO", label: "Fevereiro" },
+  { value: "MARCO", label: "Março" },
+  { value: "ABRIL", label: "Abril" },
+  { value: "MAIO", label: "Maio" },
+  { value: "JUNHO", label: "Junho" },
+  { value: "JULHO", label: "Julho" },
+  { value: "AGOSTO", label: "Agosto" },
+  { value: "SETEMBRO", label: "Setembro" },
+  { value: "OUTUBRO", label: "Outubro" },
+  { value: "NOVEMBRO", label: "Novembro" },
+  { value: "DEZEMBRO", label: "Dezembro" }
+];
