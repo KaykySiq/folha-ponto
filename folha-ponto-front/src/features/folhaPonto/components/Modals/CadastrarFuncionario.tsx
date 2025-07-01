5930d58c1b08f50c463ed952cca4953676fad49c
KaykySiq <josekayky@hotmail.com>
2025-07-01 19:30:35 -0300

    feat: Adiciona o modal de Cadastrar Funcionario.

---
+++ folha-ponto-front/src/features/folhaPonto/components/Modals/CadastrarFuncionario.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import styles from "./CadastrarFuncionario.module.css";
+import { createFuncionario } from "../../services/funcionarioService";
+
+interface Props {
+  onClose: () => void;
+  onFuncionarioCadastrado: () => void;
+}
+
+const CadastrarFuncionario = ({ onClose, onFuncionarioCadastrado }: Props) => {
+  const [name, setName] = useState("");
+  const [employeeId, setEmployeeId] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await createFuncionario({ name, employeeId });
+      setName("");
+      setEmployeeId("");
+      onFuncionarioCadastrado();
+      console.log("Funcionário cadastrado com sucesso!");
+    } catch (error) {
+      console.error("Erro ao cadastrar funcionário:", error);
+    }
+  }
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.modal}>
+        <h2>Cadastrar um novo Funcionário</h2>
+        <form className={styles.formCadastro} onSubmit={handleSubmit}> 
+            <div>
+                <label>Nome do Funcionário:</label>
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            </div>
+            <div>
+                <label>Matrícula: </label>
+                <input type="text" value={employeeId} onChange={(e) => setEmployeeId(e.target.value)} />
+            </div>
+            <button className={styles.btnCadastrar} type="submit">Cadastrar</button>
+        </form>
+        <button className={styles.closeBtn} onClick={onClose}>Fechar</button>
+      </div>
+    </div>
+  );
+};
+
+export default CadastrarFuncionario;
