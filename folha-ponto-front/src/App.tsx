bdf5b77a15245320e79fa371f76545b52a88c4a1
KaykySiq <josekayky@hotmail.com>
2025-07-01 19:28:43 -0300

    feat: Adiciona as rotas do front.

--- folha-ponto-front/src/App.tsx
+++ folha-ponto-front/src/App.tsx
@@ -1,10 +1,17 @@
-
 import './App.css'
-import TelaInicial from './features/folhaPonto/pages/TelaInicial'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TelaInicial from './features/folhaPonto/pages/TelaInicial/TelaInicial';
+import ListaFuncionarios from './features/folhaPonto/pages/ListaFuncionarios/ListaFuncionarios';
 
 function App() {
-
-  return <TelaInicial />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TelaInicial />} />
+        <Route path="/funcionarios" element={<ListaFuncionarios />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
