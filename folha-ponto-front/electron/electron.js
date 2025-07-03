1a05310c487da79af81c7ec8d222af1957e2f57b
KaykySiq <josekayky@hotmail.com>
2025-07-03 10:33:21 -0300

    chore: Configura o Electron para aplicação desktop.

---
+++ folha-ponto-front/electron/electron.js
@@ -0,0 +1,27 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1680,
+    height: 1080,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  win.loadFile(path.join(__dirname,'..', 'dist', 'index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
--- folha-ponto-front/src/App.tsx
+++ folha-ponto-front/src/App.tsx
@@ -1,16 +1,16 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import TelaInicial from './features/folhaPonto/pages/TelaInicial/TelaInicial';
 import ListaFuncionarios from './features/folhaPonto/pages/ListaFuncionarios/ListaFuncionarios';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<TelaInicial />} />
         <Route path="/funcionarios" element={<ListaFuncionarios />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
